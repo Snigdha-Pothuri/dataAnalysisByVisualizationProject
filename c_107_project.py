cd9966d8fc6ca1ac97e43a8a1bbf50a30f7df926
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-03-07 20:05:59 +0530

    Add files via upload

--- c_107_project.py
+++ c_107_project.py
@@ -4,5 +4,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 df=pd.read_csv("project_data.csv")
 print(df.groupby(["student_id", "level"], as_index=False)["attempt"].mean())
+mean=df.groupby(["student_id", "level"], as_index=False)["attempt"].mean()
 figure=px.scatter(mean,x="student_id",y="level",size="attempt", color="attempt",title="Student levels and marks on pixel math app")
 figure.show()
